1eaa2caaccd2b4a03e06543b670eb5fc1079592e
kagali11 <jakubko.ondrusek@gmail.com>
2021-05-12 16:44:01 +0200

    lol

--- fail.cpp
+++ fail.cpp
@@ -4,7 +4,7 @@ int memcheckFailDemo(int* arrayStack, unsigned int arrayStackLen,
 	int* arrayHeap, unsigned int arrayHeapLen) {
   int Stack[5];
 
-  Static[100] = 0; 
+  Static[1] = 0; 
   Stack[100] = 0; 
 
   for (int i = 0; i <= 5; i++) Stack [i] = 0;
